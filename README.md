bc70aec4c1390a7af68122c7477aa25878e0786c
Liarlee <625610036@qq.com>
2023-12-03 00:11:03 +0800

    Sync ec2 server setting to github.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ fork from suckless/dwm
 # update history
 1. 
 Sun Jun 19 12:08:27 PM CST 2022, commit from RPI4. Change README 
+
+2. 2023-12-03 00:07 - Sync the dwm from ec2 server.
+
--- config.h
+++ config.h
@@ -1,12 +1,11 @@
 /* See LICENSE file for copyright and license details. */
-
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 5;       /* snap pixel */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -14,19 +13,20 @@ static const unsigned int systrayspacing = 10;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 /* static const char *fonts[]          = { "monospace:size=10" }; */
-static const char *fonts[]          = { "Noto Sans CJK TC:size=12" };
+static const char *fonts[]          = { "Sarasa UI SC:size=12:style=Semibold Italic" };
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_gray5[]       = "#FFFFFF";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray5 },
 };
 
 /* tagging */
@@ -71,7 +71,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[] = { "alacritty", NULL };
-static const char *roficmd[] = { "rofi", "-show", "drun" };
+static const char *vimcmd[] = { "alacritty", "-e", "nvim", NULL };
+static const char *roficmd[] = { "rofi", "-show", "ssh", "-modes","ssh,drun" };
 static const char *firefoxcmd[] = { "firefox", NULL };
 static const char *copyqcmd[] = { "copyq", "toggle", NULL };
 static const char *flameshotcmd[] = { "flameshot", "gui", NULL };
@@ -83,6 +84,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = firefoxcmd } },
 	{ MODKEY,                       XK_o,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshotcmd } },
+	{ MODKEY,                       XK_v,      spawn,          {.v = vimcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_Print,  spawn,          {.v = flameshotcmd } },
 	{ MODKEY,                       XK_u,      spawn,          {.v = copyqcmd } },
